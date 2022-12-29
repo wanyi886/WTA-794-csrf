5f97b9ed6ec610bc225a809602addc4fdc01008e
wanyi <wanyile0805@gmail.com>
2022-12-28 17:02:43 -0800

    added user validation to post route

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,31 @@ const csrf = require('csurf');
 
 var csrfProtection = csrf( { cookie: true } );
 
+const users = [ { id: 1, username: "theuser", password: "abc123"} ]
 
 router.get('/', csrfProtection, function(req, res, next) {
   res.render('loginForm', { csrfToken: req.csrfToken()});
 });
 
-router.post('/process', csrfProtection, function(req, res, next){
-  res.send('Successfully Validated!')
-} )
+router.post('/session', csrfProtection, function(req, res, next){
+  
+  const { username, password } = req.body;
+
+  if (users.find( user => user.username === username && user.password === password )) {
+
+    // TODO: set session ID to cookies
+
+    res.render('loggedIn', { csrfToken: req.csrfToken()} )
+  } else {
+    // TODO: error handler
+    console.log("no")
+  }
+
+})
+
+// TODO: add get route for loggedIn page
+// TODO: add delete route
+
+
 
 module.exports = router;
